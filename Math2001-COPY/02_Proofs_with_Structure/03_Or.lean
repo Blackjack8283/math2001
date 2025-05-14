44908f6f8c54b4b1ed9e3d232872828bfb0759d0
Daichi Ito <d.ito0538@gmail.com>
2025-05-14 05:58:27 +0000

    3回目

--- Math2001-COPY/02_Proofs_with_Structure/03_Or.lean
+++ Math2001-COPY/02_Proofs_with_Structure/03_Or.lean
@@ -22,7 +22,10 @@ example {n : ℕ} : n ^ 2 ≠ 2 := by
   calc
     n ^ 2 ≤ 1 ^ 2 := by rel [hn]
     _ < 2 := by numbers
-  sorry
+  apply ne_of_gt
+  calc
+    n ^ 2 >= 2 ^ 2 := by rel [hn]
+    _ > 2 := by numbers
 
 example {x : ℝ} (hx : 2 * x + 1 = 5) : x = 1 ∨ x = 2 := by
   right
@@ -38,7 +41,12 @@ example {x : ℝ} (hx : x ^ 2 - 3 * x + 2 = 0) : x = 1 ∨ x = 2 := by
     (x - 1) * (x - 2) = x ^ 2 - 3 * x + 2 := by ring
     _ = 0 := by rw [hx]
   have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
-  sorry
+  obtain hx|hy :=h2
+  left
+  addarith[hx]
+  right
+  addarith[hy]
+
 
 example {n : ℤ} : n ^ 2 ≠ 2 := by
   have hn0 := le_or_succ_le n 0
